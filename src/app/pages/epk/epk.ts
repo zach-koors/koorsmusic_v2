365d527ca0b999365fa26013bf9ca870ae25bf59
zach-koors <67700062+zach-koors@users.noreply.github.com>
2026-05-02 19:54:19 -0400

    Add EPK Angular standalone component file.

---
+++ src/app/pages/epk/epk.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-epk',
+  standalone: true,
+  styleUrls: ['./epk.scss'],
+  templateUrl: './epk.html'
+})
+export class Epk {}
